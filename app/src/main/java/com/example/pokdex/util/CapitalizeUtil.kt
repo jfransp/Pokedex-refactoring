ed7498024c5231ebe63cbabec78b915b89438e01
jfransp <jo.santosp77@gmail.com>
2021-08-12 18:30:07 -0300

    Fixed some util functions

--- app/src/main/java/com/example/pokdex/util/CapitalizeUtil.kt
+++ app/src/main/java/com/example/pokdex/util/CapitalizeUtil.kt
@@ -1,5 +1,7 @@
 package com.example.pokdex.util
 
+//The standard "capitalize()" function seems to be deprecated for some reason and I don't know
+//what they replaced it with, so I'm just gonna set up this as not to use a deprecated function.
 fun String.capitalizeUtil () : String {
     if (this.isNotEmpty()) {
         return substring(0, 1).uppercase() + substring(1).lowercase()
