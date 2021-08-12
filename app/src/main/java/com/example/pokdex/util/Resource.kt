891ce9eb2a6b076e455a194be6b6406d4cb8b5ea
jfransp <jo.santosp77@gmail.com>
2021-08-12 08:04:13 -0300

    Comments on the yet unused resource class

--- app/src/main/java/com/example/pokdex/util/Resource.kt
+++ app/src/main/java/com/example/pokdex/util/Resource.kt
@@ -1,5 +1,10 @@
 package com.example.pokdex.util
 
+/*The resource class is not implemented - I had some trouble making it work with the paging3
+library. It might make sense to use it on the PokemonDetail request (given the fact it
+doesn't use paging), but for the sake of consistency across the code I decided not to. I'm
+keeping it here in case I can figure something out in the future (same goes for the
+responseHandler class).*/
 sealed class Resource<T>(
     val data: T? = null,
     val message: String? = null
