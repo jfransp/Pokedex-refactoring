61b6dc97bd1cc89659902f0fbbbbcdd28dba4e68
João Francisco <jfransp@protonmail.com>
2021-09-08 17:51:16 -0300

    Changed return type of getPokemonListUseCase

--- domain/src/main/java/com/example/domain/usecases/GetPokemonListUseCase.kt
+++ domain/src/main/java/com/example/domain/usecases/GetPokemonListUseCase.kt
@@ -1,11 +1,12 @@
 package com.example.domain.usecases
 
 import com.example.domain.models.pokemonlist.PokemonList
+import com.example.domain.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface GetPokemonListUseCase {
 
-    fun getPokemonList(): Flow<PokemonList> {
+    fun getPokemonList(): Resource<PokemonList> {
         TODO()
     }
 
