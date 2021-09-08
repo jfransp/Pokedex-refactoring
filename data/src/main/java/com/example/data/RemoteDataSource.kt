9ed1b8151a250d4dd8a4fd30542573f1ab8858e1
João Francisco <jfransp@protonmail.com>
2021-09-08 17:49:11 -0300

    First implementations of remoteDataSource

--- data/src/main/java/com/example/data/RemoteDataSource.kt
+++ data/src/main/java/com/example/data/RemoteDataSource.kt
@@ -1,8 +1,17 @@
 package com.example.data
 
 import com.example.data.remoteAPI.PokeApi
+import com.example.domain.models.pokemonlist.PokemonList
+import com.example.mappers.MapperImpl
+import com.example.util.ErrorHandlerImpl
 
 class RemoteDataSource(
-    val api: PokeApi
+    private val api: PokeApi,
+    private val mapper: MapperImpl,
+    private val errorHandler: ErrorHandlerImpl
 ) {
+
+    suspend fun getPokemonList(limit: Int, offset: Int): PokemonList =
+        mapper.mapPokemonListRemoteToPokemonList(api.getAllPokemon(limit, offset))
+
 }
