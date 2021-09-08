dcfcfadfb0b5b9046f807fc5444e718373edbc2c
João Francisco <jfransp@protonmail.com>
2021-09-08 16:57:56 -0300

    Changed resource class to wrap ErrorEntity object

--- domain/src/main/java/com/example/domain/util/Resource.kt
+++ domain/src/main/java/com/example/domain/util/Resource.kt
@@ -2,8 +2,8 @@ package com.example.domain.util
 
 sealed class Resource<T>(
     val data: T? = null,
-    val message: String? = null
+    val error: ErrorEntity? = null
 ) {
     class Success<T>(data: T) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+    class Error<T>(error: ErrorEntity, data: T? = null) : Resource<T>(data, error)
 }
