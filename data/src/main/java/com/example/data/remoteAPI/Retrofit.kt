39e4486ef56aa33d16c39e65846d8101fdbd199f
João Francisco <jfransp@protonmail.com>
2021-09-08 10:43:29 -0300

    Changed package structure

--- data/src/main/java/com/example/data/remoteAPI/Retrofit.kt
+++ data/src/main/java/com/example/data/remoteAPI/Retrofit.kt
@@ -1,6 +1,6 @@
 package com.example.data.remoteAPI
 
-import com.example.Constants
+import com.example.util.Constants
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
