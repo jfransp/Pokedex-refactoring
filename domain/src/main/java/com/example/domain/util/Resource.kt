20a260c48da9cbd0100a964742548bb23cd26417
João Francisco <jfransp@protonmail.com>
2021-09-08 17:49:49 -0300

    Simplified resource class

--- domain/src/main/java/com/example/domain/util/Resource.kt
+++ domain/src/main/java/com/example/domain/util/Resource.kt
@@ -1,9 +1,6 @@
 package com.example.domain.util
 
-sealed class Resource<T>(
-    val data: T? = null,
-    val error: ErrorEntity? = null
-) {
-    class Success<T>(data: T) : Resource<T>(data)
-    class Error<T>(error: ErrorEntity, data: T? = null) : Resource<T>(data, error)
+sealed class Resource<T> {
+    class Success<T>(data: T) : Resource<T>()
+    class Error<T>(error: ErrorEntity) : Resource<T>()
 }
