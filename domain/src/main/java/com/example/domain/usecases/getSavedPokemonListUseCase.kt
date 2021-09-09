62254f896d2d1133e0e85bbc3d53e80f34a157ce
João Francisco <jfransp@protonmail.com>
2021-09-09 12:15:50 -0300

    Implemented getSavedPokemonListUseCase

--- domain/src/main/java/com/example/domain/usecases/getSavedPokemonListUseCase.kt
+++ domain/src/main/java/com/example/domain/usecases/getSavedPokemonListUseCase.kt
@@ -1,9 +1,28 @@
 package com.example.domain.usecases
 
-class getSavedPokemonListUseCase {
+import com.example.domain.models.pokemondetails.PokemonDetails
+import com.example.domain.models.pokemonlist.PokemonList
+import com.example.domain.repositories.PokemonRepository
+import com.example.domain.util.ErrorHandler
+import com.example.domain.util.Resource
+import java.lang.Exception
 
-    fun getSavedPokemonList() {
+class getSavedPokemonListUseCase(
+    private val repository: PokemonRepository,
+    private val errorHandler: ErrorHandler
+) {
 
+    //The errorHandler doesn't implement local room errors yet, so it will always return an unknown
+    //error in case of an exception
+    suspend fun getSavedPokemonList(): Resource<List<PokemonDetails>> {
+        val output: Resource<List<PokemonDetails>> = try {
+            val result = repository.getPokemonDetailsListFromLocal()
+            Resource.Success(result)
+        } catch (e: Exception) {
+            val error = errorHandler.getError(e)
+            Resource.Error(error)
+        }
+        return output
     }
 
 }
