eaa7b287de6098f85333269d991ea324a2496694
João Francisco <jfransp@protonmail.com>
2021-09-10 17:19:04 -0300

    Fixed error message selector utility function to use context

--- app/src/main/java/com/example/pokdex/util/ErrorMessageSelector.kt
+++ app/src/main/java/com/example/pokdex/util/ErrorMessageSelector.kt
@@ -1,17 +1,17 @@
 package com.example.pokdex.util
 
-import android.content.res.Resources
+import android.content.Context
 import com.example.domain.util.ErrorEntity
 import com.example.pokdex.R
 
-fun selectErrorMessageFromErrorEntity(error: ErrorEntity): String {
+fun selectErrorMessageFromErrorEntity(context: Context, error: ErrorEntity): String {
     return when (error) {
-        is ErrorEntity.Network -> Resources.getSystem().getString(R.string.network_error_message)
-        is ErrorEntity.NotFound -> Resources.getSystem().getString(R.string.not_found_error_message)
-        is ErrorEntity.AccessDenied -> Resources.getSystem().getString(R.string.access_denied_error_message)
-        is ErrorEntity.ServiceUnavailable -> Resources.getSystem().getString(R.string.service_unavailable_error_message)
-        is ErrorEntity.RequestTimedOut -> Resources.getSystem().getString(R.string.request_timed_out_error_message)
-        is ErrorEntity.Unknown -> Resources.getSystem().getString(R.string.unkown_error_message)
-        else -> Resources.getSystem().getString(R.string.unkown_error_message)
+        is ErrorEntity.Network -> context.getString(R.string.network_error_message)
+        is ErrorEntity.NotFound -> context.getString(R.string.not_found_error_message)
+        is ErrorEntity.AccessDenied -> context.getString(R.string.access_denied_error_message)
+        is ErrorEntity.ServiceUnavailable -> context.getString(R.string.service_unavailable_error_message)
+        is ErrorEntity.RequestTimedOut -> context.getString(R.string.request_timed_out_error_message)
+        is ErrorEntity.Unknown -> context.getString(R.string.unkown_error_message)
+        else -> context.getString(R.string.unkown_error_message)
     }
 }
